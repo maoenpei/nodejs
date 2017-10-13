45a3e9d06d3161b4a2fc4858e4a24ac7bedd397a
git_user <patronumme@gmail.com>
2017-10-13 21:22:51 +0800

    able to list players in king war

--- server/yzdzz/GameController.js
+++ server/yzdzz/GameController.js
@@ -10,10 +10,12 @@ Base.extends("GameConnection", {
         this.password = password;
         this.accountInfo = null;
         this.servers = [];
-        this.sock = null;
+        this.serverInfo = null;
         this.gameInfo = null;
-        this.recvCallbacks = {};
 
+        this.sock = null;
+
+        this.recvCallbacks = {};
         this.commonCallbacks = {};
         this.events = {};
         this.quickRegs = {};
@@ -22,6 +24,18 @@ Base.extends("GameConnection", {
 
     on:function(type, callback) {
         this.events[type] = callback;
+    },
+    getAccountInfo:function() {
+        return this.accountInfo;
+    },
+    getServers:function() {
+        return this.servers;
+    },
+    getServerInfo:function() {
+        return this.serverInfo;
+    },
+    getGameInfo:function() {
+        return this.gameInfo;
     },
 
     quit:function() {
@@ -34,7 +48,7 @@ Base.extends("GameConnection", {
             // login request
             var obj = yield GameHTTP.login(this.username, this.password, next);
             if (obj.code != 'SUCCESS') {
-                return safe(done)(false);
+                return safe(done)({});
             }
             this.accountInfo = {
                 accessToken : obj.value.accessToken,
@@ -78,7 +92,9 @@ Base.extends("GameConnection", {
                 }
             }
 
-            safe(done)(true);
+            safe(done)({
+                success: true,
+            });
         }, this);
     },
     servers:function() {
@@ -87,11 +103,11 @@ Base.extends("GameConnection", {
     loginGame:function(desc, done) {
         var next = coroutine(function*() {
             if (!this.accountInfo) {
-                return safe(done)(false);
+                return safe(done)({});
             }
             // already login, must quit first
             if (this.sock) {
-                return safe(done)(false);
+                return safe(done)({});
             }
 
             var server = null;
@@ -103,15 +119,16 @@ Base.extends("GameConnection", {
                 }
             }
             if (!server) {
-                return safe(done)(false);
+                return safe(done)({});
             }
 
             var sock = yield GameSock.connect(server.ip, server.port, next);
             if (!sock) {
-                return safe(done)(false);
+                return safe(done)({});
             }
             console.log("Connected with ip:{0}, port:{1}, server:{2}".format(server.ip, server.port, server.desc));
             this.sock = sock;
+            this.serverInfo = server;
             GameSock.receive(sock, (c, m, data) => {
                 this.onReceive(c, m, data);
             });
@@ -140,6 +157,7 @@ Base.extends("GameConnection", {
             if (result != 'done') {
                 console.log("stat failed playerId:{0} error:{1}".format(this.gameInfo.playerId, result));
             }
+            console.log("Player id:{0}, name:{1}".format(this.gameInfo.playerId, this.gameInfo.name));
             //var obj = yield GameHTTP.save(this.accountInfo.accountId, this.gameInfo.playerId, server.serverId, this.accountInfo.accessToken, next);
             //if (obj.code != 'SUCCESS') {
             //    console.log("save failed accountId:{0} code:{1} reason:{2}".format(this.accountInfo.playerId, obj.code, obj.desc));
@@ -149,23 +167,12 @@ Base.extends("GameConnection", {
                 console.log("loginServer failed accountId:{0} result:{1}".format(this.accountInfo.accountId, result));
             }
 
-            safe(done)(true);
+            safe(done)({
+                success:true,
+            });
         }, this);
     },
-    testProtocol:function(done) {
-        var next = coroutine(function*() {
-            //var data = yield this.sendMsg("Sign", "getinfo", null, next);
-            //var data = yield this.sendMsg("Collect", "getinfo", null, next);
-            //var data = yield this.sendMsg("RoleExplore", "update", {type:'', login:1}, next); // 点击收集
-            //var data = yield this.sendMsg("ActLevelGift", "getinfo", null, next); // 等级奖励
-            //var data = yield this.sendMsg("ActGoldenHero", "getinfo", null, next); // 金币魔女
-            //var data = yield this.sendMsg("ActCatchup", "info", null, next); // 后来居上
-            //var data = yield this.sendMsg("UnionWar", "usecard", {}, next);
-            //yield $FileManager.saveFile("/recvdata.json", JSON.stringify(data), next);
-            safe(done)();
-        }, this);
-    },
-    getRoleInfo:function(done) {
+    getRole:function(done) {
         var next = coroutine(function*() {
             var data = yield this.sendMsg("Role", "getInfo", null, next);
             // act_splendid: 福利
@@ -179,34 +186,53 @@ Base.extends("GameConnection", {
             safe(done)();
         }, this);
     },
-    getUnionInfo:function(done) {
+    getVisiblePlayers:function() {
         var next = coroutine(function*() {
-            var data = yield this.sendMsg("Union", "getinfo", null, next);
-            var result = {
-                unionId : data.id,
-                ownerId : data.owner,
-            };
-            safe(done)(result);
+            var data = yield this.sendMsg("KingWar", "roleRank", null, next);
+            var players = [];
+            for (var i = 0; i < data.list.length; ++i) {
+                var item = data.list[i];
+                players.push({
+                    playerId: item.uid,
+                    power: item.cpi,
+                    name: item.role_name,
+                    union: item.server + "." + item.union_name,
+                });
+            }
+            safe(done)({
+                players: players,
+            });
         }, this);
     },
+    getUnion:function(done) {
+        var next = coroutine(function*() {
+            var data = yield this.sendMsg("Union", "getinfo", null, next);
+            safe(done)({
+                unionId : data.id,
+                ownerId : data.owner,
+            });
+        }, this);
+    },
+
     getUnionWar:function(done) {
         var next = coroutine(function*() {
             var data = yield this.sendMsg("UnionWar", "getinfo", null, next);
-            var result = Number(data.open) == 1;
-            safe(done)(result);
+            var isOpen = Number(data.open) == 1;
+            safe(done)({
+                isOpen: isOpen,
+            });
         }, this);
     },
     enterUnionWar:function(landId, done) {
         var next = coroutine(function*() {
             var data = yield this.sendMsg("UnionWar", "enter", null, next);
             if (!data) {
-                return safe(done)(null);
+                return safe(done)({});
             }
             var data = yield this.sendMsg("UnionWar", "enterland", {id:landId}, next);
             if (!data) {
-                return safe(done)(null);
+                return safe(done)({});
             }
-            console.log("land info - ", data);
             var mineArray = [];
             for (var i = 0; i < data.gems.length; ++i) {
                 var gem = data.gems[i];
@@ -216,57 +242,165 @@ Base.extends("GameConnection", {
                     unionId: gem.union_id,
                 });
             }
-            console.log("card info", data.card_id, data.card_used);
-            var result = {
+            safe(done)({
                 cardReady: Number(data.card_used) == 0,
-                cardType: Number(data.card_id == 8),
+                cardType: data.card_id,
+                isGood: data.card_id <= 4,
                 mineArray: mineArray,
-            };
-            safe(done)(result);
-        }, this);
-    },
-    occupy:function(landId, pos, done) {
-        var next = coroutine(function*() {
-            var ckey = this.regMsg("UnionWar", "occupy", (data) => {
-                console.log("occupy result:", data);
-                if (!data) {
-                    next(false);
-                } else if (data.id == landId && data.pos == pos) {
-                    next(true);
-                }
             });
-            yield this.sendNotify("UnionWar", "occupy", {id:landId, pos:pos}, next);
-            var result = yield; // wait for msg received
-            this.unregMsg(ckey);
-            safe(done)(result);
         }, this);
     },
     getCardList:function(done) {
         var next = coroutine(function*() {
-            var result = [];
-            yield this.sendMsg("UnionWar", "cardinfo", null, next);
+            var data = yield this.sendMsg("UnionWar", "cardinfo", null, next);
             if (!data || !data.target || data.target.length == 0) {
-                return safe(done)(result);
+                return safe(done)({});
             }
+            var cardList = [];
             for (var i = 0; i < data.target.length; ++i) {
                 var target = data.target[i];
-                result.push({
+                cardList.push({
                     unionId: target.id,
                     name: target.name,
                     good: target.buff,
                     bad: target.debuff,
                 });
             }
-            safe(done)(result);
+            safe(done)({
+                cardList: cardList,
+            });
+        }, this);
+    },
+    occupy:function(landId, pos, done) {
+        var next = coroutine(function*() {
+            var ckey = this.regMsg("UnionWar", "occupy", (data) => {
+                if (!data) {
+                    next(null);
+                } else if (data.id == landId && data.pos == pos) {
+                    next(data);
+                }
+            });
+            yield this.sendNotify("UnionWar", "occupy", {id:landId, pos:pos}, next);
+            var data = yield; // wait for msg received
+            this.unregMsg(ckey);
+            if (!data) {
+                return safe(done)({});
+            }
+            safe(done)({
+                playerId: data.owner,
+                unionId: data.union_id,
+            });
         }, this);
     },
     useCard:function(unionId, done) {
         var next = coroutine(function*() {
             var data = yield this.sendMsg("UnionWar", "usecard", {id:unionId}, next);
             if (!data) {
-                return safe(done)(false);
+                return safe(done)({});
             }
-            safe(done)(true);
+            safe(done)({
+                success:true,
+            });
+        }, this);
+    },
+
+    getKingWar:function(done) {
+        var next = coroutine(function*() {
+            var data = yield this.sendMsg("KingWar", "getinfo", null, next);
+            safe(done)({
+                allowJoin: data.state == 1,
+                joined: data.check != 0,
+                inlist: data.check == 1,
+            });
+        }, this);
+    },
+    getArea:function(areaId, done) {
+        var next = coroutine(function*() {
+            var data = yield this.sendMsg("KingWar", "getAreaInfo", null, next);
+            var starNumbers = [];
+            for (var i = 0; i < data.lands.length; ++i) {
+                starNumbers.push(data[i].num);
+            }
+            safe(done)({
+                starNumbers : starNumbers,
+            });
+        }, this);
+    },
+    joinKingWar:function(areaId, starId, done) {
+        var next = coroutine(function*() {
+            var data = yield this.sendMsg("KingWar", "apply", {areaid:areaId, star:starId}, next);
+            if (!data) {
+                return safe(done)({});
+            }
+            if (areaId != data.areaid || starId != data.star) {
+                console.log("Join KingWar (area:{0}, star:{1}) error:".format(areaId, starId), data);
+            }
+            safe(done)({
+                success: true,
+            });
+        }, this);
+    },
+    getKingWarPlayers:function(done) {
+        var next = coroutine(function*() {
+            var data = yield this.sendMsg("KingWar", "getLandInfo", null, next);
+            if (!data) {
+                return safe(done)({});
+            }
+            var players = [];
+            for (var i = 0; i < data.list.length; ++i) {
+                var item = data.list[i];
+                players.push({
+                    playerId: item.uid,
+                    power: item.cpi,
+                    name: item.role_name,
+                });
+            }
+            safe(done)({
+                players: players,
+            });
+        }, this);
+    },
+    getRace:function(done) {
+        var next = coroutine(function*() {
+            var data = yield this.sendMsg("KingWar", "getRaceInfo", null, next);
+            if (!data) {
+                return safe(done)({});
+            }
+            var players = [];
+            for (var i = 0; i < data.list.length; ++i) {
+                var item = data.list[i];
+                players.push({
+                    playerId: item.uid,
+                    name: item.role_name,
+                });
+            }
+            safe(done)({
+                players: players,
+            });
+        }, this);
+    },
+    testProtocol:function(done) {
+        var next = coroutine(function*() {
+            console.log(new Date().getTime() / 1000);
+            var data = null;
+            //var data = yield this.sendMsg("Collect", "getinfo", null, next);
+            //var data = yield this.sendMsg("Chat", "all", null, next); // 聊天信息
+            //var data = yield this.sendMsg("Chat", "logs", null, next); // 
+            //var data = yield this.sendMsg("RoleExplore", "update", {type:'', login:1}, next); // 点击收集
+            //var data = yield this.sendMsg("RoleExplore", "balance", null, next);
+            //var data = yield this.sendMsg("ActLevelGift", "getinfo", null, next); // 等级奖励
+            //var data = yield this.sendMsg("ActGoldenHero", "getinfo", null, next); // 金币魔女
+            //var data = yield this.sendMsg("ActCatchup", "info", null, next); // 后来居上
+            //var data = yield this.sendMsg("UnionWar", "cardlog", null, next); // 查看卡牌列表
+            //var data = yield this.sendMsg("UnionWar", "ahead", null, next); // 查看名次信息
+            //var data = yield this.sendMsg("UnionWar", "refreshCard", null, next); // 刷新可用卡牌
+            //var data = yield this.sendMsg("KingWar", "areaRank", {areaid:1}, next);
+
+            //var data = yield this.sendMsg("Sign", "getinfo", null, next); // 打卡
+            //var data = yield this.sendMsg("Sign", "start", {point:0}, next); // 打卡，今天
+            console.log(data);
+            yield $FileManager.saveFile("/../20170925_yongzhe_hack/recvdata.json", JSON.stringify(data), next);
+            safe(done)();
         }, this);
     },
 
@@ -331,7 +465,7 @@ Base.extends("GameConnection", {
         if (!callbackArray) {
             return;
         }
-        for (var i = 0; i < callbackArray; ++i) {
+        for (var i = 0; i < callbackArray.length; ++i) {
             if (reg.callback === callbackArray[i]) {
                 callbackArray.splice(i, 1);
                 break;
@@ -343,6 +477,10 @@ Base.extends("GameConnection", {
         this.regMsg("MsgBox", "message", (data) => {});
         this.regMsg("Role", "kick", (data) => { this.quit(); safe(this.events["break"])(); });
         this.regMsg("UnionRace", "notify", (data) => {});
+        this.regMsg("UnionWar", "kill", (data) => {});
+        this.regMsg("UnionWar", "sync", (data) => {});
+        this.regMsg("UnionWar", "occupy", (data) => {});
+        this.regMsg("UnionWar", "leave", (data) => {});
     },
 });
 
