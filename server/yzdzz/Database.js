f680aa1703df88d06d67943e45c7de78a7549732
git_user <patronumme@gmail.com>
2017-11-24 19:32:36 +0800

    change data base

--- server/yzdzz/Database.js
+++ server/yzdzz/Database.js
@@ -4,18 +4,20 @@ require("../Base");
 Database = {};
 
 Database.meterial = {
-    "book":{name:"勇者契约书", },
-    "evolve":{name:"主角进化卡", },
-    "food_0":{name:"梅子饭团", },
-    "food_1":{name:"爱心便当", },
-    "food_2":{name:"魔能汉堡", },
-    "food_3":{name:"泡椒喵蛋", },
-    "food_4":{name:"龙肉", },
-    "stone_1":{name:"蓝色随机石碎片", },
-    "stone_2":{name:"紫色随机石碎片", },
-    "stone_3":{name:"橙色随机石碎片", },
-    "stone_4":{name:"红色随机石碎片", },
-    "stone_5":{name:"金色随机石碎片", },
+    "summon_book":{name:"勇者契约书", },
+    "league_war_horn":{name:"国战笑脸", },
+    "hero_upgrade_card":{name:"主角进化卡", },
+    "hero_upgrade_card_piece":{name:"主角进化卡碎片", },
+    "food_2":{name:"梅子饭团", },
+    "food_3":{name:"爱心便当", },
+    "food_4":{name:"魔能汉堡", },
+    "food_5":{name:"泡椒喵蛋", },
+    "food_6":{name:"龙肉", },
+    "stone_piece_random_3":{name:"蓝神石碎片", },
+    "stone_piece_random_4":{name:"紫神石碎片", },
+    "stone_piece_random_5":{name:"橙神石碎片", },
+    "stone_piece_random_6":{name:"红神石碎片", },
+    "stone_piece_random_7":{name:"金神石碎片", },
 };
 
 Database.goblin = {
